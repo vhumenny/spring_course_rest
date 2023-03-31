00ba417d0b303339d4debd999683fda037bd6058
vhumenny <v.humenny@gmail.com>
2023-03-31 23:29:37 +0300

    rest api

---
+++ src/main/java/com/zaurtregulov/spring/rest/exception_handling/EmployeeIncorrectData.java
@@ -0,0 +1,12 @@
+package com.zaurtregulov.spring.rest.exception_handling;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class EmployeeIncorrectData {
+    private String info;
+}
