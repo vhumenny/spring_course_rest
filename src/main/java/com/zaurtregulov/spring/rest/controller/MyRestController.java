2563f13b71093d3c1639bc9ed75a1dde37877474
vhumenny <v.humenny@gmail.com>
2023-04-01 11:13:46 +0300

    finished

--- src/main/java/com/zaurtregulov/spring/rest/controller/MyRestController.java
+++ src/main/java/com/zaurtregulov/spring/rest/controller/MyRestController.java
@@ -35,4 +35,23 @@ public class MyRestController {
 
         return employee;
     }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+        if (employee==null) {
+            throw new NoSuchEmployeeException("There is no employee with id = " + id + " in Database.");
+        }
+
+        employeeService.deleteEmployee(id);
+        return "Employee with id = " + id + " was deleted";
+
+    }
 }
